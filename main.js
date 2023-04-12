06410104c304df8af4aa635bcb0c400014b19324
jaque1108 <56333861+jaque1108@users.noreply.github.com>
2023-04-12 09:12:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 function responsiveMenu(){
 
-var x = document.getElementById('nav')
-if(x.className === ""){
-x.className = "responsive"
-} else{
-x.className = "";
-}
+	var x = document.getElementById('nav')
+	if(x.className === ""){
+		x.className = "responsive"
+	} else{
+		x.className = "";
+	}
 }
 
 function seleccionar(link){
-var opciones = document.querySelectorAll('#links a');
-opciones[0].className = "";
-opciones[1].className = "";
-opciones[2].className = "";
-opciones[3].className = "";
-opciones[4].className = "";
-link.className = "seleccionado";
+	var opciones = document.querySelectorAll('#links a');
+	opciones[0].className = "";
+	opciones[1].className = "";
+	opciones[2].className = "";
+	opciones[3].className = "";
+	opciones[4].className = "";
+	link.className = "seleccionado";
 
-var x = document.getElementById("nav");
-x.className = "";
+	var x = document.getElementById("nav");
+	x.className = "";
 
 }
